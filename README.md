33a96975c351100fc6d9c491424dbcbc5cb8b6af
Sergio RA <Sergrojas29@gmail.com>
2024-12-12 00:32:20 -0500

    intital commit

---
+++ README.md
@@ -0,0 +1,7 @@
+### KMEANS AND USING SKLEARN
+
+## INSTALL
+
+Using a virtual enviroment in jupyter  labs.
+
+https://scikit-learn.org/1.5/install.html
---
+++ test.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import make_blobs
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import random
+import numpy as np
+from sklearn.metrics.pairwise import euclidean_distances
+
+import matplotlib.pyplot as plt
+
